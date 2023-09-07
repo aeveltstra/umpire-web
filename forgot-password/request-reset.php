fce11ef5870dddc9e1274e3ea10d405cf89bdaa4
Œnðr <aev@sdf.org>
2023-09-06 22:18:26 -0400

    added more parts of the authentication flow

---
+++ forgot-password/request-reset.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1)
+
+/**
+ * Request new credentials to access Umpire
+ * @author A.E.Veltstra for OmegaJunior Consultancy
+ * @version 20230906t2125
+ */
+header('Location: ./received/')
+?>
+
