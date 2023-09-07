f90ddf3b57cd5acc27f032c9cb038d7fdcd02980
Œnðr <aev@sdf.org>
2023-09-06 23:48:45 -0400

    syntax

--- forgot-password/request-reset.php
+++ forgot-password/request-reset.php
@@ -6,7 +6,24 @@ declare(strict_types=1);
  * @author A.E.Veltstra for OmegaJunior Consultancy
  * @version 20230906t2125
  */
-header('Location: ./received/');
-die();
+
+$reset_email_tainted = $_POST['email'];
+session_start();
+$_SESSION['reset_email_tainted'] = $reset_email_tainted;
+
+$is_valid = filter_var(
+    $reset_email_tainted, 
+    FILTER_VALIDATE_EMAIL,
+    FILTER_FLAG_EMAIL_UNICODE
+);
+
+if ($is_valid) {
+    
+    header('Location: ./received');
+    die();
+} else {
+    header('Location: ./invalid-email');
+    die();
+}
 ?>
 
--- sign-up/request.php
+++ sign-up/request.php
@@ -1,11 +1,30 @@
 <?php
-declare(strict_types=1)
+declare(strict_types=1);
 
 /**
  * Request access to Umpire
  * @author A.E.Veltstra for OmegaJunior Consultancy
  * @version 20230906t2125
  */
-header('Location: ./received/')
+$email_tainted = $_POST['email'];
+$reason_tainted = $_POST['reason'];
+session_start();
+$_SESSION['add_email_tainted'] = $email_tainted;
+$_SESSION['add_reason_tainted'] = $reason_tainted;
+
+$is_valid = filter_var(
+    $email_tainted, 
+    FILTER_VALIDATE_EMAIL,
+    FILTER_FLAG_EMAIL_UNICODE
+);
+
+if ($is_valid) {
+    
+    header('Location: ./received');
+    die();
+} else {
+    header('Location: ./invalid-email');
+    die();
+}
 ?>
 
