032e7784281ade6345cae206fe669a1f9b38a5f9
Œnðr <aev@sdf.org>
2023-09-06 22:11:25 -0400

    created additional entry screens

---
+++ sign-up/request.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1)
+
+/**
+ * Request access to Umpire
+ * @author A.E.Veltstra for OmegaJunior Consultancy
+ * @version 20230906t2125
+ */
+header('Location: ./received/')
+?>
+
