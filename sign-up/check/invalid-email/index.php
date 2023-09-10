6051bf0e8178240fbf8c1f26b9f76fd210acda26
Œnðr <aev@sdf.org>
2023-09-10 01:25:46 -0400

    Correcting variable names

--- sign-up/check/invalid-email/index.php
+++ sign-up/check/invalid-email/index.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 session_start();
-$add_email_valid = $_SESSION['add_email_valid'];
+$add_email_invalid = $_SESSION['add_email_invalid'];
 $add_reason_tainted = $_SESSION['add_reason_tainted'];
 ?>
 <!DOCTYPE html>
@@ -16,9 +16,10 @@ $add_reason_tainted = $_SESSION['add_reason_tainted'];
 <form method=post action="../check">
 <fieldset><legend>I want an account to gain special access to Umpire.</legend>
 <p><label for=email>My E-mail:</label></p>
-<p><input type=email name=email id=email value="<?=htmlspecialchars($last_email_tainted)?>" size=50/></p>
+<p><input type=email name=email id=email value="<?=htmlspecialchars($add_email_invalid)?>" size=50/></p>
 <p><label for=reason>My reason for wanting access:</label></p>
-<p><textarea name=reason id=reason cols=60 rows=10><?=htmlspecialchars($last_reason_tainted)?></textarea></p>
+<p><textarea name=reason id=reason cols=60
+rows=10><?=htmlspecialchars($add_reason_tainted)?></textarea></p>
 <p><label><input type=submit value="Request Access"/></label></p>
 </fieldset>
 </form>
