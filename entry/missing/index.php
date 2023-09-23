7bf3de4cb2cd86ecac42e51c57a50fcf894401a3
Œnðr <aev@sdf.org>
2023-09-22 22:54:56 -0400

    Now having enumeration data lists for enumerated input lists.

--- entry/missing/index.php
+++ entry/missing/index.php
@@ -1,29 +1,48 @@
 <?php
+declare(strict_types=1);
 /**
  * Shows a case entry form. The fields are generated on the fly
- * based on the fiels listed in the database.
+ * based on the fields listed in the database.
  * @author A.E.Veltstra
- * @version 2.23.909.1834
+ * @version 2.23.919.2229
  */
-
-include_once (dirname(__FILE__) . '/../../config.php');
+include_once '../../config.php';
 
 /**
  * Reads the enumerations from the database. They are stored as
  * separate values for each attribute, with a language code.
  */
 function read_enumerations_from_db() {
-
+    $sql = "select `attribute_id`, `enum_value` from `enums` where `language_code` = 'en' order by `attribute_id`";
+    return query($sql);
 }
+
 
 /**
  * Reads the enumerations from the database and generates a single
  * input constraint for each attribute.
  */
 function show_enums() {
-
+  
+    $xs = read_enumerations_from_db();
+    
+    $a = '';
+    $m = '';
+    foreach($xs as $x) {
+        [$b, $e] = $x;
+        if ($a != $b) {
+            if ($m) {
+                $n = '<datalist id="list_' . addslashes($a) . '">' . $m . '</datalist>';
+                echo $n;
+            }
+            $a = $b;
+            $m = '';
+        }
+        $v = addslashes($e);
+        $m .= '<option value="' . $v . '">' . $v . '</option>';
+    }
+    
 }
-
 
 /**
  * Reads the database fields and their attributes, so they can be
@@ -38,20 +57,10 @@ function show_enums() {
  * ]
  */
 function read_form_fields() {
-    global $eraskcsstufgyc, $vhjilfyhkkot, $bjkyfvbnkiyf, $yaefgvcaoelo;
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    $mysqli = new mysqli($eraskcsstufgyc, $vhjilfyhkkot, $bjkyfvbnkiyf, $yaefgvcaoelo);
-    $sql = 'SELECT `id`, `data_type`, `caption`, `hint`, `min`, `max`, `hide_on_entry` FROM `vw_missing_entry_form_attributes` order by `display_sequence` asc'; 
-    $result = $mysqli->query($sql, MYSQLI_STORE_RESULT);
-    $buffer = $result->fetch_all(MYSQLI_BOTH);
-    $mock = [
-        ['$id' => 'aliases', 'data_type' => 'shorttext', '$caption' => 'Other known names', '$hint' => 'Nick names, government names, pen names, etc.'],
-        ['$id' => 'birth year', 'data_type' => 'integer', '$caption' => 'Born in which year?', '$hint' => 'Include the century.'],
-        ['$id' => 'last seen on date', 'data_type' => 'date', '$caption' => 'Last Seen Date', '$hint' => 'To your best knowledge, at what date was this person seen last?'],
-        ['$id' => 'last seen date accuracy', 'data_type' => 'percent', '$caption' => 'Accuracy of the Last Seen Date', '$hint' => 'On a scale of 0 to 100%, how accurate is the date at which this person was last seen?']
-    ];
-    return $buffer;
+    $sql = 'SELECT `id`, `data_type`, `caption`, `hint`, `min`, `max`, `hide_on_entry` FROM `vw_missing_entry_form_attributes_en` order by `display_sequence` asc'; 
+    return query($sql);
 }
+
 
 /**
  * Generates HTML for the form fields and echoes the result
@@ -59,13 +68,9 @@ function read_form_fields() {
  */
 function show_entry_fields() {
     $templates = array(
-        'shorttext' => '<fieldset><legend>%3$s</legend><p><label
-        for="%1$s">%4$s</label></p><p><input type=text size=60
-        minlength="%5$d" maxlength="%6$d" name="%1$s" id="%1$s"/></p></fieldset>',
+        'shorttext' => '<fieldset><legend>%3$s</legend><p><label for="%1$s">%4$s</label></p><p><input type=text size=60 minlength="%5$d" maxlength="%6$d" name="%1$s" id="%1$s"/></p></fieldset>',
         'integer' => '<fieldset><legend>%3$s</legend><p><label for="%1$s">%4$s</label></p><p><input type=number min="%5$d" max="%6$d" name="%1$s" id="%1$s"/></p></fieldset>',
-        'enum' => '<fieldset><legend>%3$s</legend><p><label
-        for="%1$s">%4$s</label></p><p><input type=text size=60
-        minlength="%5$d" maxlength="%6$d" name="%1$s" id="%1$s"/></p></fieldset>',
+        'enum' => '<fieldset><legend>%3$s</legend><p><label for="%1$s">%4$s</label></p><p><input type=text size=60 minlength="%5$d" maxlength="%6$d" name="%1$s" id="%1$s" list="list_%1$s"/></p></fieldset>',
         'date' => '<fieldset><legend>%3$s</legend><p><label for="%1$s">%4$s</label></p><p><input type=date name="%1$s" id="%1$s"/></p></fieldset>',
         'longtext' => '<fieldset><legend>%3$s</legend><p><label for="%1$s">%4$s</label></p><p><textarea cols=60 rows=10 maxlength="%6$d" name="%1$s" id="%1$s"></textarea></p></fieldset>',
         'percent' => '<fieldset><legend>%3$s</legend><p><label for="%1$s">%4$s</label></p><p><input type=number min=0 max=100 name="%1$s" id="%1$s"/></p></fieldset>'
@@ -81,18 +86,22 @@ function show_entry_fields() {
     }
 }
 
+
 ?>
 <!DOCTYPE html>
 <html lang=en encoding="utf-8">
 <head>
-<title>New Case Entry - Umpire</title>
+<title>New Missing Person's Case Entry - Umpire</title>
 <meta name=description value="Please share as many details as available"/>
 </head>
 <body>
-<h1>New Case Entry - Umpire</h1>
+<h1>New Missing Person's Case Entry - Umpire</h1>
 <h2>Please share as many details as available</h2>
 <form action=register method=post>
-    <?php show_entry_fields(); ?>
+    <?php 
+        show_enums();
+        show_entry_fields(); 
+    ?>
     <fieldset>
         <legend>Done!</legend>
         <p><label><input type=checkbox name=agree /> I accept </label><a
