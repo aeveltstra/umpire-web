6fa8f98afcdf9bb64520236ac3205761eb98a125
Ænðr E. Feldstraw <aev@sdf.org>
2023-09-27 06:54:13 -0400

    Added agreement passthrough

--- sign-up/check/index.php
+++ sign-up/check/index.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 /**
  * Request access to Umpire
  * @author A.E.Veltstra for OmegaJunior Consultancy
- * @version 2.23.927.646
+ * @version 2.23.927.0015
  */
 $email_tainted = $_POST['email'];
 $reason_tainted = $_POST['reason'];
+$agreed = $_POST['agree'];
 
 $is_valid = filter_var(
     $email_tainted, 
@@ -16,16 +17,17 @@ $is_valid = filter_var(
 );
 
 session_start();
+$_SESSION['add_user_reason_tainted'] = $reason_tainted;
+$_SESSION['add_user_agreed_tainted'] = $agreed;
+
 if ($is_valid) {
-    $_SESSION['add_email_valid'] = $email_tainted;
-    $_SESSION['add_reason_tainted'] = $reason_tainted;
+    $_SESSION['add_user_email_valid'] = $email_tainted;
     header('Location: ./valid-email');
+
     die();
 } else {
-    $_SESSION['add_email_invalid'] = $email_tainted;
-    $_SESSION['add_reason_tainted'] = $reason_tainted;
+    $_SESSION['add_user_email_invalid'] = $email_tainted;
     header('Location: ./invalid-email');
     die();
 }
 ?>
-
