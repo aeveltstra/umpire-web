62267761e7ff07e9f059e194c554b8fd0e92609d
Œnðr <aev@sdf.org>
2023-09-27 20:22:22 -0400

    Remember that server redirects break POSTs

--- sign-up/check/invalid-email/index.php
+++ sign-up/check/invalid-email/index.php
@@ -1,8 +1,10 @@
 <?php
 declare(strict_types=1);
+error_reporting(E_ALL);
 session_start();
-$add_email_invalid = $_SESSION['add_email_invalid'];
-$add_reason_tainted = $_SESSION['add_reason_tainted'];
+
+$add_user_email_invalid = $_SESSION['add_user_email_invalid'];
+$add_user_reason_tainted = $_SESSION['add_user_reason_tainted'];
 ?>
 <!DOCTYPE html>
 <html lang=en>
@@ -15,14 +17,14 @@ $add_reason_tainted = $_SESSION['add_reason_tainted'];
 <body>
 <h1>Error: invalid e-mail - Sign up - Umpire</h1>
 <h2>That did not look like a valid e-mail address. Try again.</h2>
-<form method=post action="../check">
+<form method=post action="../check/">
 <fieldset><legend>I want special access to Umpire.</legend>
 <p><label for=email>My E-mail:</label></p>
 <p><input type=email name=email id=email
-value="<?=htmlspecialchars($add_email_invalid);?>" size=60/></p>
+value="<?php echo htmlspecialchars($add_user_email_invalid); ?>" size=60/></p>
 <p><label for=reason>My reason for wanting access:</label></p>
 <p><textarea name=reason id=reason cols=60
-rows=5><?=htmlspecialchars($add_reason_tainted);?></textarea></p>
+rows=5><?php echo htmlspecialchars($add_user_reason_tainted); ?></textarea></p>
 </fieldset>
 <fieldset><legend>Options</legend>
 <p><label><input type=checkbox name=agree /> I have read and agree to </label><a href="../../terms/">the terms and conditions</a></p>
@@ -31,3 +33,4 @@ rows=5><?=htmlspecialchars($add_reason_tainted);?></textarea></p>
 </form>
 </body>
 </html>
+
