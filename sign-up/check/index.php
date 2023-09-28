16a36f7e518621cfb11875bbe27b2e5b6cbc935b
Œnðr <aev@sdf.org>
2023-09-27 20:19:32 -0400

    Remember that server redirects break POSTs

--- sign-up/check/index.php
+++ sign-up/check/index.php
@@ -1,14 +1,23 @@
-<?php
+<?php 
 declare(strict_types=1);
+error_reporting(E_ALL);
+$started = session_start();
 
 /**
  * Request access to Umpire
  * @author A.E.Veltstra for OmegaJunior Consultancy
  * @version 2.23.927.0015
  */
+/* 
+echo var_dump($_POST);
+echo "\nSession started: ${started}\n";
+*/
 $email_tainted = $_POST['email'];
 $reason_tainted = $_POST['reason'];
 $agreed = $_POST['agree'];
+
+$_SESSION['add_user_reason_tainted'] = $reason_tainted;
+$_SESSION['add_user_agreed_tainted'] = $agreed;
 
 $is_valid = filter_var(
     $email_tainted, 
@@ -16,18 +25,19 @@ $is_valid = filter_var(
     FILTER_FLAG_EMAIL_UNICODE
 );
 
-session_start();
-$_SESSION['add_user_reason_tainted'] = $reason_tainted;
-$_SESSION['add_user_agreed_tainted'] = $agreed;
-
 if ($is_valid) {
     $_SESSION['add_user_email_valid'] = $email_tainted;
-    header('Location: ./valid-email');
+session_write_close();
+    header('Location: ./valid-email/');
 
-    die();
+    exit();
 } else {
     $_SESSION['add_user_email_invalid'] = $email_tainted;
-    header('Location: ./invalid-email');
-    die();
+session_write_close();
+    header('Location: ./invalid-email/');
+    exit();
 }
+/* echo var_dump($_SESSION); */
 ?>
+
+
