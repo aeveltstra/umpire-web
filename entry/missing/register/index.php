c71ad681f17d9a628acdb416f94ada8dd0294ad9
Œnðr <aev@sdf.org>
2023-10-08 18:13:03 -0400

    Started the script that stores the missing case entry

---
+++ entry/missing/register/index.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+/**
+ * Stores the missing person's case entry. Fields are generated on 
+ * the fly based on the fields listed in the database.
+ * @author A.E.Veltstra
+ * @version 2.23.1008.1811
+ */
+error_reporting(E_ALL);
+require_once $SERVER['DOCUMENT_ROOT'] . '/umpire/db_utils.php';
+
+header('Location: ./success/');
+die;
+
+?>
