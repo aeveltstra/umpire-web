48564ae599f805874caa8b1b83e31f4919034950
Ænðr E. Feldstraw <aev@sdf.org>
2024-04-29 22:42:59 -0400

    Make sure to post to and redirect to directories.

--- reset-secrets/check/index.php
+++ reset-secrets/check/index.php
@@ -1,11 +1,14 @@
-<?php
+<?php 
 declare(strict_types=1);
 
 /**
  * Request new credentials to access Umpire
  * Step 1: check whether the passed-in email is considered valid.
+ * 
+ * PHP Version 7.5.3
+ * 
  * @author  A.E.Veltstra for OmegaJunior Consultancy <omegajunior@protonmail.com>
- * @version 2.23.909.2224
+ * @version 2.24.429.2239
  */
 
 $reset_email_tainted = $_POST['email'];
@@ -18,12 +21,13 @@ $is_valid = filter_var(
 session_start();
 if ($is_valid) {
     $_SESSION['reset_email_valid'] = $reset_email_tainted;
-    header('Location: ./valid-email');
+    header('Location: ./valid-email/');
     die();
 } else {
     $_SESSION['reset_email_tainted'] = $reset_email_tainted;
-    header('Location: ./invalid-email');
+    header('Location: ./invalid-email/');
     die();
 }
 ?>
+
 
