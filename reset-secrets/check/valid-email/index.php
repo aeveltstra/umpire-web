6704cb591920524cc70b9cc7726a955b20a6bead
Ænðr E. Feldstraw <aev@sdf.org>
2024-04-29 23:29:13 -0400

    chose better name for function

--- reset-secrets/check/valid-email/index.php
+++ reset-secrets/check/valid-email/index.php
@@ -7,8 +7,10 @@ declare(strict_types=1);
  * 
  * PHP Version 7.5.3.
  * 
- * @author  A.E.Veltstra for OmegaJunior Consultancy <omegajunior@protonmail.com>
- * @version 2.24.429.1043
+ * @category Administrative
+ * @package  Umpire
+ * @author   A.E.Veltstra for OmegaJunior Consultancy <omegajunior@protonmail.com>
+ * @version  2.24.429.1043
  */
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/umpire/session_utils.php';
@@ -17,7 +19,7 @@ $reset_email_valid = session_recall('reset_email_valid');
 $is_known = db_is_email_known($reset_email_valid);
 
 if ($is_known) {
-    $reset_key = set_and_get_reset_key($reset_email_valid);
+    $reset_key = db_make_authentication_reset_key($reset_email_valid);
     session_remember('known_email', $reset_email_valid);
     session_rememember('reset_key', $reset_key);
     header('Location: ./send/');
