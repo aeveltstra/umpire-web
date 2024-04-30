501595e7e034239ccaa8d5c029beb7c9ece085c1
Ænðr E. Feldstraw <aev@sdf.org>
2024-04-29 23:19:42 -0400

    Switch to session_utils.

--- reset-secrets/check/index.php
+++ reset-secrets/check/index.php
@@ -1,6 +1,5 @@
 <?php 
 declare(strict_types=1);
-
 /**
  * Request new credentials to access Umpire
  * Step 1: check whether the passed-in email is considered valid.
@@ -11,6 +10,7 @@ declare(strict_types=1);
  * @version 2.24.429.2239
  */
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/umpire/session_utils.php';
 $reset_email_tainted = $_POST['email'];
 $is_valid = filter_var(
     $reset_email_tainted, 
@@ -18,13 +18,12 @@ $is_valid = filter_var(
     FILTER_FLAG_EMAIL_UNICODE
 );
 
-session_start();
 if ($is_valid) {
-    $_SESSION['reset_email_valid'] = $reset_email_tainted;
+    session_remember('reset_email_valid', $reset_email_tainted);
     header('Location: ./valid-email/');
     die();
 } else {
-    $_SESSION['reset_email_tainted'] = $reset_email_tainted;
+    session_remember('reset_email_tainted', $reset_email_tainted);
     header('Location: ./invalid-email/');
     die();
 }
--- reset-secrets/check/valid-email/index.php
+++ reset-secrets/check/valid-email/index.php
@@ -1,28 +1,31 @@
 <?php
 declare(strict_types=1);
-
 /**
  * Request new credentials to access Umpire
  * Step 2: check whether the passed-in email is a known user,
  * and if so, set a temporary reset key.
+ * 
+ * PHP Version 7.5.3.
+ * 
  * @author  A.E.Veltstra for OmegaJunior Consultancy <omegajunior@protonmail.com>
- * @version 2.23.909.2224
+ * @version 2.24.429.1043
  */
 
-session_start();
-$reset_email_valid = $_SESSION['reset_email_valid'];
-$is_known = is_email_known($reset_email_valid);
+require_once $_SERVER['DOCUMENT_ROOT'] . '/umpire/session_utils.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/umpire/db_utils.php';
+$reset_email_valid = session_recall('reset_email_valid');
+$is_known = db_is_email_known($reset_email_valid);
 
 if ($is_known) {
     $reset_key = set_and_get_reset_key($reset_email_valid);
-    $_SESSION['known_email'] = $reset_email_valid;
-    $_SESSION['reset_key'] = $reset_key;
-    header('Location: ./send');
+    session_remember('known_email', $reset_email_valid);
+    session_rememember('reset_key', $reset_key);
+    header('Location: ./send/');
     die();
 } else {
-    $_SESSION['known_email'] = '';
-    $_SESSION['reset_key'] = '';
-    header('Location: ./sent');
+    session_forget('known_email');
+    session_forget('reset_key');
+    header('Location: ./sent/');
     die();
 }
 ?>
