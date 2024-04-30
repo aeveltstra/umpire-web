baeba2d4fb83e132396abc3468391cc0f16efc5f
Ænðr E. Feldstraw <aev@sdf.org>
2024-04-30 19:45:26 -0400

    Formatting, mostly.

--- reset-secrets/check/valid-email/send/index.php
+++ reset-secrets/check/valid-email/send/index.php
@@ -1,11 +1,15 @@
 <?php
 declare(strict_types=1);
-
 /**
  * Request new credentials to access Umpire
  * Step 3: send reset to email of known user.
- * @author  A.E.Veltstra for OmegaJunior Consultancy <omegajunior@protonmail.com>
- * @version 2.23.909.2234
+ * 
+ * PHP Version 7.5.3
+ * 
+ * @category Administrative
+ * @package  Umpire
+ * @author   A.E.Veltstra for OmegaJunior Consultancy <omegajunior@protonmail.com>
+ * @version  2.24.430.1936
  */
 
 session_start();
@@ -14,18 +18,22 @@ $reset_key = $_SESSION['reset_key'];
 $success = mail(
     $known_email,
     'Umpire: set your access credentials',
-    "Hello,\r\n\r\nUse the below link to set your credentials to access
-    the database for Unidentified and Missing People, Independent
-    Research and
-    Education.\r\n\r\nhttps://www.umpi.re/set-access/?reset_key=${reset_key}&email=${known_email}\r\n\r\n"
+    "Hello,\r\n
+    \r\n
+    Use the below link to set your credentials to access the database 
+    for Unidentified and Missing People, Independent Research and
+    Education.\r\n
+    \r\n
+    https://www.umpi.re/set-access/?reset_key=${reset_key}&email=${known_email}
+    \r\n
+    \r\n"
 );
 
-
 if ($success) {
-    header('Location: ../sent');
+    header('Location: ../sent/');
     die();
 } else {
-    header('Location: ./failed-to-send');
+    header('Location: ./failed-to-send/');
     die();
 }
 ?>
