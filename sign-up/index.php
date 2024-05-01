0b795a834a21c28e6d7d3e666bbf8577e65e3441
Ænðr E. Feldstraw <aev@sdf.org>
2024-04-30 20:44:44 -0400

    Improved wording.

--- sign-up/index.php
+++ sign-up/index.php
@@ -1,10 +1,15 @@
 <?php
 /**
  * Account creation form.
- * @author  A.E.Veltstra for OmegaJunior Consultancy <omegajunior@protonmail.com>
- * @version 2.23.1024.2214
+ * 
+ * PHP Version 7.5.3
+ * 
+ * @category Administrative
+ * @package  Umpire
+ * @author   A.E.Veltstra for OmegaJunior Consultancy <omegajunior@protonmail.com>
+ * @version  2.24.430.2039
  */
-include_once $_SERVER['DOCUMENT_ROOT'] . '/umpire/session_utils.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/umpire/session_utils.php';
 $did_authenticate = session_did_user_authenticate();
 $form_nonce = session_make_and_remember_nonce('sign_up_form');
 ?>
@@ -19,46 +24,63 @@ $form_nonce = session_make_and_remember_nonce('sign_up_form');
 <body>
 <h1>Sign up - Umpire</h1>
 <h2>Ask us to create an account for you</h2>
-<p>Remember that anyone can enter cases. The process is anonymous. You do not need an account to enter cases. Reasons for getting an account, include:</p>
+<p>Remember that anyone can enter cases. The process is anonymous. You do
+    not need an account to enter cases. Reasons for getting an account, 
+    include:</p>
 <ul><li>Reviewing cases already entered;</li>
     <li>Correcting existing cases;</li>
-    <li>Comparing cases of missing people against those of deceased people;</li>
+    <li>Comparing cases of missing people against those of deceased 
+        people;</li>
     <li>Exporting our case lists as a spreadsheet;</li>
     <li>And more!</li>
 </ul>
-<p>Your request WILL be reviewed by one of our operatives. Access will NOT be granted automatically. Argue your need to gain access to our system. Please bear in mind our operatives are volunteers. They'll get around to your request as soon as possible.</p>
+<p>Your request WILL be reviewed by one of our operatives. Access will 
+    NOT be granted automatically. Argue your need to gain access to our 
+    system. Please bear in mind our operatives are volunteers. They'll 
+    get around to your request as soon as possible.</p>
 <form method=post action="check/">
 <?php
-    if ($form_nonce) {
-        echo "    <input type=hidden value='${form_nonce}' name=nonce />\r\n";
-    }
+if ($form_nonce) {
+    echo "<input type=hidden value='${form_nonce}' name=nonce />\r\n";
+}
 ?>
     <fieldset><legend>Terms of Use</legend>
-        <p><label><input type=checkbox name=agree />I have read and agree to the <a href="terms/">terms of use</a>.</label></p>
+        <p><label><input 
+            type=checkbox 
+            name=agree />I have read and agree to the <a 
+            href="terms/">terms of use</a>.</label>
+        </p>
     </fieldset>
     <fieldset><legend>I want special access to Umpire.</legend>
         <p><label for=email>My E-mail Address:</label></p>
         <p><input type=email name=email id=email value="" size=60 /></p>
-        <p><label for=reason>My reason for wanting access:</label></p>
 <?php
-    if ($did_authenticate) {
-      echo "        <p class=warning>Warning: you appear to be logged 
+if ($did_authenticate) {
+    echo "<p class=warning>Warning: you appear to be logged 
             in. Please explain why you would need a second user account?
             </p>\r\n";
-    }
+} else {
+    echo "<p><label for=reason>My reason for wanting access:</label></p>";
+}
 ?>
         <p><textarea name=reason id=reason cols=60 rows=5></textarea></p>
     </fieldset>
     <fieldset><legend>Step 1 of 2</legend>
-        <p><label><input type=submit value="Check my E-mail Address"/></label></p>
+        <p><label><input 
+            type=submit 
+            value="Check my E-mail Address"/></label>
+        </p>
     </fieldset>
 </form>
 <?php if (!$did_authenticate) { ?>
-<form method=get action="../sign-in/">
-    <fieldset><legend>Already have an account?</legend>
-        <p><label><input type=submit value="Yes, let me sign in!"/></label></p>
-    </fieldset>
-</form>
+    <form method=get action="../sign-in/">
+        <fieldset><legend>Already have an account?</legend>
+            <p><label><input 
+                type=submit 
+                value="Yes, let me sign in!"/></label>
+            </p>
+        </fieldset>
+    </form>
 <?php } ?>
 </body>
 </html>
