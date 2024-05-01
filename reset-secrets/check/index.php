16fd4e47f8148a0c271e823453893f723913145a
Ænðr E. Feldstraw <aev@sdf.org>
2024-04-30 20:12:47 -0400

    Added security checks

--- reset-secrets/check/index.php
+++ reset-secrets/check/index.php
@@ -6,12 +6,40 @@ declare(strict_types=1);
  * 
  * PHP Version 7.5.3
  * 
- * @author  A.E.Veltstra for OmegaJunior Consultancy <omegajunior@protonmail.com>
- * @version 2.24.429.2239
+ * @category Administrative
+ * @package  Umpire
+ * @author   A.E.Veltstra for OmegaJunior Consultancy <omegajunior@protonmail.com>
+ * @version  2.24.430.1948
  */
+
+/**
+ * If this process gets invoked by any method other than HTTP POST,
+ * processing needs to halt and the user needs to be redirected.
+ * The process requires to be invoked using HTTP POST.
+ */
+if (!isset($_SERVER['REQUEST_METHOD'])) {
+    http_response_code(400);
+    die();
+}
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(400);
+    die();
+}
+if (!isset($_POST['nonce'])) {
+    header('Location: ./error-wrong-form/');
+    die();
+}
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/umpire/session_utils.php';
 $reset_email_tainted = $_POST['email'];
+$form_nonce = $_POST['nonce'];
+$form_id = 'authentication_reset_form';
+if (!session_is_nonce_valid($form_id)) {
+    header('Location: ./error-wrong-form/');
+    die();
+}
+session_forget_nonce($form_id);
+
 $is_valid = filter_var(
     $reset_email_tainted, 
     FILTER_VALIDATE_EMAIL,
